8a24c5a5df71d85dc1dd7114de33897576a9c506
RAMENtheNOODLES <rommecar000@bdusd.org>
2024-02-08 22:15:34 -0600

    Changes for 2.2.4

--- kubejs/server_scripts/other_recipes.js
+++ kubejs/server_scripts/other_recipes.js
@@ -1,28 +1,28 @@
-ServerEvents.recipes(event => {
-    event.shaped(
-        Item.of('minecraft:cobweb', 1),
-        [
-            ' S ',
-            'SSS',
-            ' S '
-        ],
-        {
-            S: 'minecraft:string'
-        }
-    )
+ServerEvents.recipes((event) => {
+  event.shaped(Item.of("minecraft:cobweb", 1), [" S ", "SSS", " S "], {
+    S: "minecraft:string",
+  });
 
-    event.shaped(
-        Item.of('walkietalkie:diamond_walkietalkie', 1),
-        [
-            'CD',
-            'RI',
-            'II'
-        ],
-        {
-            C: 'minecraft:lightning_rod',
-            D: 'minecraft:diamond_block',
-            R: 'minecraft:redstone_block',
-            I: 'minecraft:iron_block'
-        }
-    )
-})
+  event.shaped(
+    Item.of("walkietalkie:diamond_walkietalkie", 1),
+    ["CD", "RI", "II"],
+    {
+      C: "minecraft:lightning_rod",
+      D: "minecraft:diamond_block",
+      R: "minecraft:redstone_block",
+      I: "minecraft:iron_block",
+    },
+  );
+
+  event.shaped(
+    Item.of("walkietalkie:iron_walkietalkie", 1),
+    ["CG", "RI", "II"],
+    {
+      C: "minecraft:lightning_rod",
+      G: "minecraft:gold_block",
+      R: "minecraft:redstone",
+      I: "minecraft:iron_block",
+    },
+  );
+});
+
